7d4c2536a21f6d5431a734939f808d1d210c66d2
QuangAnh <qh1403@gmai.com>
2026-06-18 14:18:24 +0700

    fuckoff autosave

--- be/src/route/rental.route.js
+++ be/src/route/rental.route.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const { getAllRentals, createRental, updateRentalStatus } = require('../controller/rental.controller');
-const { verifyToken } = require('../middleware/auth');
-=======
 const { getAllRentals, createRental, updateRentalStatus, deleteRental } = require('../controller/rental.controller');
->>>>>>> bfc8b0aebcee2e7709f7e5ed257316133154ef40
 
 router.get('/', getAllRentals);
 router.post('/',verifyToken, createRental);
