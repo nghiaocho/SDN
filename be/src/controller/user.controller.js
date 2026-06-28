e481e7a7a29353c82e4eee62331c350a60d62edf
QuangAnh <qh1403@gmai.com>
2026-06-29 00:06:53 +0700

    Create: AuthContext, get current user information api

--- be/src/controller/user.controller.js
+++ be/src/controller/user.controller.js
@@ -39,8 +39,22 @@ const login = async (req, res) => {
     }
 };
 
+const getInfo = async(req, res) =>{
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        if(!user){
+            res.status(404).json({message: "User not found!"})
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
 module.exports = {
     getAllUsers,
     register,
-    login
+    login,
+    getInfo
 };
